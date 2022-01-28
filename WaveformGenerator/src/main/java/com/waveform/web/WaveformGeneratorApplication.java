6bc0f4dee81ba7421b0de783730ac3d048fa71e4
jakehowden <jakehowden@gmail.com>
2022-01-28 15:47:00 +0000

    Added base spring project

---
+++ WaveformGenerator/src/main/java/com/waveform/web/WaveformGeneratorApplication.java
@@ -0,0 +1,13 @@
+package com.waveform.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaveformGeneratorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaveformGeneratorApplication.class, args);
+	}
+
+}
---
+++ WaveformGenerator/src/test/java/com/waveform/web/WaveformGeneratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waveform.web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WaveformGeneratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
