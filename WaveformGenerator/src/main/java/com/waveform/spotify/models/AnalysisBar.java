5244e18fe59fad2a5760768d2bf132b7d5b5ae30
jakehowden <jakehowden@gmail.com>
2022-01-31 18:53:22 +0000

    Added all models for Track Analysis and finished the AnalyseTrack method

---
+++ WaveformGenerator/src/main/java/com/waveform/spotify/models/AnalysisBar.java
@@ -0,0 +1,52 @@
+package com.waveform.spotify.models;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonPropertyOrder({
+  "start",
+  "duration",
+  "confidence"
+})
+public class AnalysisBar {
+
+  @JsonProperty("start")
+  private Double start;
+  @JsonProperty("duration")
+  private Double duration;
+  @JsonProperty("confidence")
+  private Double confidence;
+
+  @JsonProperty("start")
+  public Double getStart() {
+    return start;
+  }
+
+  @JsonProperty("start")
+  public void setStart(Double start) {
+    this.start = start;
+  }
+
+  @JsonProperty("duration")
+  public Double getDuration() {
+    return duration;
+  }
+
+  @JsonProperty("duration")
+  public void setDuration(Double duration) {
+    this.duration = duration;
+  }
+
+  @JsonProperty("confidence")
+  public Double getConfidence() {
+    return confidence;
+  }
+
+  @JsonProperty("confidence")
+  public void setConfidence(Double confidence) {
+    this.confidence = confidence;
+  }
+
+}
